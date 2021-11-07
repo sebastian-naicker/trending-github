ae4ad496d27c6bdeee8a1051734d47ece224ccaa
Sebastian Naicker <sebastian.naicker@johnlewis.co.uk>
2021-11-07 10:42:03 +0200

    chore: install deps

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './index.scss';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>
+      hello
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
