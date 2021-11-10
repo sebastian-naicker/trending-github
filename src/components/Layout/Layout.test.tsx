203521cff4c06644aef1da52537ddadd328dacf4
Sebastian Naicker <sebastian.naicker@johnlewis.co.uk>
2021-11-10 17:24:52 +0200

    feat: add layout

---
+++ src/components/Layout/Layout.test.tsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react'
+import Layout from './Layout'
+
+describe('Layout', function () {
+  it('should render Layout without errors', function () {
+    render(<Layout />)
+  })
+})
