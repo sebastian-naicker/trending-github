8c8d109ae0b03ad21d342e1c066aa60c9149542d
Sebastian Naicker <sebastian.naicker@johnlewis.co.uk>
2021-11-10 18:53:53 +0200

    feat: update header and footer

---
+++ src/components/Footer/Footer.test.tsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react'
+import Footer from './Footer'
+
+describe('Footer', function () {
+  it('should render without errors', function () {
+    render(<Footer />)
+  })
+})
---
+++ src/components/Header/Header.test.tsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react'
+import Header from './Header'
+
+describe('Header', function () {
+  it('should render without errors', function () {
+    render(<Header />)
+  })
+})
