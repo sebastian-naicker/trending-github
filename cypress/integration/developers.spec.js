0e0f8053e5a75e9857729a5b5447d580111510df
Sebastian Naicker <sebastian.naicker@johnlewis.co.uk>
2021-11-19 08:36:08 +0200

    feat: add cypress tests for devlopers and repositories

--- cypress/integration/developers.spec.js
+++ cypress/integration/developers.spec.js
@@ -1,15 +1,16 @@
 /// <reference types="cypress" />
 
-describe('Trending Repositories', () => {
+describe('Trending Developers', () => {
   beforeEach(() => {
     // Cypress starts out with a blank slate for each test
     // so we must tell it to visit our website with the `cy.visit()` command.
     // Since we want to visit the same URL at the start of all our tests,
     // we include it in our beforeEach function so that it runs before each test
-    cy.visit('http://localhost:3000/')
+    cy.visit('http://localhost:3000/developers')
   })
 
-  it('displays most popular repositories', () => {
-    cy.get('.todo-list li').should('have.length', 2)
+  it('displays most popular developers', () => {
+    cy.get('[data-testid=developers-page]').should('have.length', 1)
+    cy.get('[data-testid=table-list] > li').should('have.length', 2)
   })
 })
---
+++ cypress/integration/repositories.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe('Trending Repositories', () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit('http://localhost:3000/')
+  })
+
+  it('displays most popular repositories', () => {
+    cy.get('[data-testid=table-list] > li').should('have.length', 2)
+  })
+})
